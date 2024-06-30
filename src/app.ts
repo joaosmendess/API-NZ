2f6ef0b0108f8cb30d4cc67e7a3aa04fd544f715
Gustavo <avoguga@gmail.com>
2024-06-30 14:27:21 -0300

    feat: deu ruim

--- src/app.ts
+++ src/app.ts
@@ -22,13 +22,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// Middleware para configurar COOP e COEP
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  next();
-});
-
 // Mock simple authentication
 const mockUsers = [
   { username: 'nucleo_zero', password: 'd3fT7g8k' },
